f8c06526f3821f701b18d5a1626567a9c674eb09
omar-eaa <143701116+omar-eaa@users.noreply.github.com>
2024-05-16 22:38:45 +0200

    working on index.html page

--- js/index.js
+++ js/index.js
@@ -36,3 +36,22 @@ omOsNavContainer.addEventListener("mouseleave", () => {
   // Hide the omOs nav container
   omOsNavContainer.style.display = "none";
 });
+
+// thank you message 📧👇
+// Get the button element and message container
+const subscribeButton = document.getElementById("subscribeButton");
+const messageContainer = document.getElementById("messageContainer");
+const emailSection = document.getElementById("emailSection");
+// Hide the message container by default
+messageContainer.style.display = "none";
+
+// Add a click event listener to the button
+subscribeButton.addEventListener("click", function () {
+  // Show the message container
+  messageContainer.style.display = "block";
+
+  // Hide the message container after 3 seconds
+  setTimeout(function () {
+    messageContainer.style.display = "none";
+  }, 3000);
+});
